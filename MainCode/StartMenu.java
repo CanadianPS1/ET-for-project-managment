e82ce5dfe1964f537dbe5371de05db6cef89ecc0
Lia <122105453+CanadianPS1@users.noreply.github.com>
2025-01-15 09:45:29 -0700

    Update StartMenu.java

--- MainCode/StartMenu.java
+++ MainCode/StartMenu.java
@@ -1,22 +1,41 @@
 package MainCode;
 
-import java.awt.event.ActionListener;
-
+import java.awt.event.*;
 import javax.swing.*;
 
-public class StartMenu {
+public class StartMenu implements ActionListener{
+    JFrame game;
+    ImageIcon startScreenIcon;
+    JButton startButton;
     public StartMenu(){
-        ImageIcon startScreenIcon = new ImageIcon("");
-        JFrame game = new JFrame();
+        startScreenIcon = new ImageIcon("seperated sprites\\Screens\\MainMenue.png");
+
+
+
+        game = new JFrame();
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        game.setSize(320,210);
+        game.setSize(335,249);
         game.setVisible(true);
-        JButton startButton = new JButton();
+        startButton = new JButton();
+        startButton.setSize(320,210);
         startButton.setVisible(true);
-        startButton.setOpaque(true);
+        startButton.setIcon(startScreenIcon);
+
+        //game.add(startScreenLabel);
         game.add(startButton);
+
 
         game.getGraphics();
         startButton.addActionListener((ActionListener) this);
+    
+        
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        startButton.setVisible(false);
+        game.getGraphics();
+        System.out.println("Start Button Pressed");
+    }
+    
 }
