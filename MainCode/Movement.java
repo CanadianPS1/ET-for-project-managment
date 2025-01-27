20cb21585fb55e936371daa766071b12db8f5651
Lia <sethroeback1@gmail.com>
2025-01-27 09:34:46 -0700

    almost finished movment

--- MainCode/Movement.java
+++ MainCode/Movement.java
@@ -12,6 +12,10 @@ public class Movement implements KeyListener{
     ImageIcon ETMoveOne = new ImageIcon("seperated sprites\\E.T\\ETWalk01.png");
     ImageIcon ETMoveTwo = new ImageIcon("seperated sprites\\E.T\\ETWalk02.png");
     ImageIcon backgroundTestIcon = new ImageIcon("seperated sprites\\Screens\\Pits01.png");
+    ImageIcon ETFlyOne = new ImageIcon("seperated sprites\\E.T\\ETStretch01.png");
+    ImageIcon ETFlyTwo = new ImageIcon("seperated sprites\\E.T\\ETStretch02.png");
+    ImageIcon ETFlyThree = new ImageIcon("seperated sprites\\E.T\\ETStretch03.png");
+    
     public Movement(JFrame g, int nS){
         game = g;
         nextScreen = nS;
@@ -52,25 +56,56 @@ public class Movement implements KeyListener{
 
         return ET;
     }
+    public JLabel ETFlyAnimation(JLabel ET){
+        switch (moveAnimationSpot) {
+            case 0 -> {
+                ET.setIcon(ETFlyOne);
+                moveAnimationSpot = 1;
+            }
+            case 1 -> {
+                ET.setIcon(ETFlyTwo);
+                moveAnimationSpot = 2;
+            }
+            case 2 ->{
+                ET.setIcon(ETFlyThree);
+                moveAnimationSpot = 3;
+            }
+            default -> {
+                ET.setIcon(ETFlyThree);
+            }
+        }
+
+        return ET;
+    }
 
  
     @Override
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
-        switch (key) {
-            case KeyEvent.VK_W -> ET.setLocation(ET.getX(), ET.getY() - 5);
-            case KeyEvent.VK_A -> ET.setLocation(ET.getX() - 5, ET.getY());
-            case KeyEvent.VK_S -> ET.setLocation(ET.getX(), ET.getY() + 5);
-            case KeyEvent.VK_D -> ET.setLocation(ET.getX() + 5, ET.getY());
+        if(key == KeyEvent.VK_W){
+            ETMoveAnimation(ET);
+            ET.setLocation(ET.getX(), ET.getY() - 5);
+        }else if(key == KeyEvent.VK_A){
+            ET.setLocation(ET.getX() - 5, ET.getY());
+            ETMoveAnimation(ET);
+        }else if(key == KeyEvent.VK_S){
+            ET.setLocation(ET.getX(), ET.getY() + 5);
+            ETMoveAnimation(ET);
+        }else if(key == KeyEvent.VK_D){
+            ET.setLocation(ET.getX() + 5, ET.getY());
+            ETMoveAnimation(ET);
+        }else if(key == KeyEvent.VK_SPACE){
+            ET.setLocation(ET.getX(), ET.getY() - 5);
+            ETFlyAnimation(ET);
         }
-        ETMoveAnimation(ET);
+        moveAnimationSpot = 0;
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
         System.out.println("KEY RELEACED: " + key);
-        if (key == KeyEvent.VK_W || key == KeyEvent.VK_A || key == KeyEvent.VK_S || key == KeyEvent.VK_D) {
+        if (key == KeyEvent.VK_W || key == KeyEvent.VK_A || key == KeyEvent.VK_S || key == KeyEvent.VK_D || key == KeyEvent.VK_SPACE) {
             ET.setIcon(ETIdle);
         }
     }
