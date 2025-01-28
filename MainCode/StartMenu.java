efe160e4029b375e4b7335d67bfbb2eb2199175e
Lia <sethroeback1@gmail.com>
2025-01-28 10:31:31 -0700

    Update StartMenu.java

--- MainCode/StartMenu.java
+++ MainCode/StartMenu.java
@@ -1,19 +1,18 @@
+//makes the starting screen and lets you click it
 package MainCode;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-
 public class StartMenu implements ActionListener{
     protected JFrame game;
     private final ImageIcon startScreenIcon;
     private final JButton startButton;
+    //makes the JFrame and puts a button in it so you can start the game
     public StartMenu(){
         startScreenIcon = new ImageIcon("seperated sprites\\Screens\\MainMenue.png");
         ImageIcon gameIcon = new ImageIcon("seperated sprites\\E.T\\ETIdle.png");
-
         game = new JFrame();
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         game.setBounds(700,300,325,240);
@@ -24,12 +23,12 @@ public class StartMenu implements ActionListener{
         startButton.setSize(320,210);
         startButton.setVisible(true);
         startButton.setIcon(startScreenIcon);
-        
         game.add(startButton);
-
         game.update(game.getGraphics());
         startButton.addActionListener((ActionListener) this);       
     }
+    //the method for when you click the button
+    //it disables its self then makes ET and all that jazz
     @Override
     public void actionPerformed(ActionEvent e){
         startButton.setVisible(false);
@@ -37,9 +36,7 @@ public class StartMenu implements ActionListener{
         game.update(game.getGraphics());
         System.out.println("Start Button Pressed");
         new Movement(game).ETMoveFirstRun();
-
         SceneHandler handler = new SceneHandler(game, 0);
-
         handler.screenChange();
     }  
 }
