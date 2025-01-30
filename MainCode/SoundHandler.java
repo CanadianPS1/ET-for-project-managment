999139f06d4f9d49ecac62791ffb694b51b66206
Izzy :3 <60242236+MRL1002@users.noreply.github.com>
2025-01-29 20:09:06 -0700

    added main menu music, turns off when menu is exited

---
+++ MainCode/SoundHandler.java
@@ -0,0 +1,32 @@
+package MainCode;
+
+import java.io.File;
+import javax.sound.sampled.*;
+public class SoundHandler {
+
+    public Clip soundControl(String strPath, boolean blnIsOn){
+
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(strPath).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            return clip;
+        } catch (Exception e) {
+            System.out.println("Error with playing sound.");
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public void sounndOff(Clip clip){
+        try {
+            clip.close();
+        } catch (Exception e) {
+            System.out.println("Error with playing sound.");
+            e.printStackTrace();
+        }
+    }
+    
+}
--- MainCode/StartMenu.java
+++ MainCode/StartMenu.java
@@ -2,6 +2,7 @@
 package MainCode;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.sound.sampled.Clip;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,6 +10,7 @@ public class StartMenu implements ActionListener{
     protected JFrame game;
     private final ImageIcon startScreenIcon;
     private final JButton startButton;
+    protected Clip strSound;
     //makes the JFrame and puts a button in it so you can start the game
     public StartMenu(){
         startScreenIcon = new ImageIcon("seperated sprites\\Screens\\MainMenue.png");
@@ -25,7 +27,13 @@ public class StartMenu implements ActionListener{
         startButton.setIcon(startScreenIcon);
         game.add(startButton);
         game.update(game.getGraphics());
-        startButton.addActionListener((ActionListener) this);       
+        startButton.addActionListener((ActionListener) this);
+
+        String strSoundPath = "Sounds\\MainMenu.wav";
+
+        SoundHandler soundHandler = new SoundHandler();
+        strSound = soundHandler.soundControl(strSoundPath, true);
+
     }
     //the method for when you click the button
     //it disables its self then makes ET and all that jazz
@@ -36,6 +44,9 @@ public class StartMenu implements ActionListener{
         game.update(game.getGraphics());
         System.out.println("Start Button Pressed");
         new Movement(game).ETMoveFirstRun();
+
+        SoundHandler soundHandler = new SoundHandler();
+        soundHandler.sounndOff(strSound);
 
 
         @SuppressWarnings("unused")
