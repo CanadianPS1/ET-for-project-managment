3c9a01f3a282341ca4400fe738603facf5b50a3b
Izzy :3 <8mrl1002@gmail.com>
2025-02-03 10:06:42 -0700

    fixed last push

--- MainCode/Movement.java
+++ MainCode/Movement.java
@@ -165,7 +165,7 @@ public class Movement implements KeyListener{
 
         location = handler.detectLREdge(ET, location, energyUI);
         location = handler.detectUDEdge(ET, location, energyUI);
-        handler.detectpit(ET, key);
+        handler.detectpit(ET, location, energyUI);
     }
     @Override
     public void keyReleased(KeyEvent e) {
