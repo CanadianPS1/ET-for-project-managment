3b53ab3df85d6e767c62727116cfd7802242e48a
Lia <sethroeback1@gmail.com>
2025-02-04 10:04:44 -0700

    converted to switch

--- MainCode/PhonePieceSpawns.java
+++ MainCode/PhonePieceSpawns.java
@@ -11,94 +11,106 @@ public class PhonePieceSpawns extends Movement{
         phonePiece2Map = rand.nextInt(1,5);
         phonePiece3Map = rand.nextInt(1,5);
         //generates the locations for the first phone piece
-        if(phonePiece1Map == 1){
-            phonePiece1Hole = rand.nextInt(1,5);
-            phonePiece1Map =  rand.nextInt(1,3);
-            if(phonePiece1Map == 1){
-                phonePiece1Map = 8;
-            }else{
-                phonePiece1Map = 9;
+        switch (phonePiece1Map) {
+            case 1 -> {
+                phonePiece1Hole = rand.nextInt(1,5);
+                phonePiece1Map =  rand.nextInt(1,3);
+                if(phonePiece1Map == 1){
+                    phonePiece1Map = 8;
+                }else{
+                    phonePiece1Map = 9;
+                }
             }
-        }else if(phonePiece1Map == 2){
-            phonePiece1Hole = rand.nextInt(1,5);
-            phonePiece1Map =  rand.nextInt(1,3);
-            if(phonePiece1Map == 1){
-                phonePiece1Map = 3;
-            }else{
-                phonePiece1Map = 7;
+            case 2 -> {
+                phonePiece1Hole = rand.nextInt(1,5);
+                phonePiece1Map =  rand.nextInt(1,3);
+                if(phonePiece1Map == 1){
+                    phonePiece1Map = 3;
+                }else{
+                    phonePiece1Map = 7;
+                }
             }
-        }else if(phonePiece1Map == 3){
-            phonePiece1Hole = rand.nextInt(1,5);
-            phonePiece1Map =  rand.nextInt(1,3);
-            if(phonePiece1Map != 1){
-                phonePiece1Map = 2;
+            case 3 -> {
+                phonePiece1Hole = rand.nextInt(1,5);
+                phonePiece1Map =  rand.nextInt(1,3);
+                if(phonePiece1Map != 1){
+                    phonePiece1Map = 2;
+                }
             }
-        }else if(phonePiece1Map == 4){
-            phonePiece1Hole = rand.nextInt(1,9);
-            phonePiece1Map = 5;
-        }else{
-            System.err.println("ERROR: invalid map id at phone piece 1 Map spawns");
+            case 4 -> {
+                phonePiece1Hole = rand.nextInt(1,9);
+                phonePiece1Map = 5;
+            }
+            default -> System.err.println("ERROR: invalid map id at phone piece 1 Map spawns");
         }
 
 
         //generates the locations for the second phone piece
-        if(phonePiece2Map == 1){
-            phonePiece2Hole = rand.nextInt(1,5);
-            phonePiece2Map =  rand.nextInt(1,3);
-            if(phonePiece2Map == 1){
-                phonePiece2Map = 8;
-            }else{
-                phonePiece2Map = 9;
+        switch (phonePiece2Map) {
+            case 1 -> {
+                phonePiece2Hole = rand.nextInt(1,5);
+                phonePiece2Map =  rand.nextInt(1,3);
+                if(phonePiece2Map == 1){
+                    phonePiece2Map = 8;
+                }else{
+                    phonePiece2Map = 9;
+                }
             }
-        }else if(phonePiece2Map == 2){
-            phonePiece2Hole = rand.nextInt(1,5);
-            phonePiece2Map =  rand.nextInt(1,3);
-            if(phonePiece2Map == 1){
-                phonePiece2Map = 3;
-            }else{
-                phonePiece2Map = 7;
+            case 2 -> {
+                phonePiece2Hole = rand.nextInt(1,5);
+                phonePiece2Map =  rand.nextInt(1,3);
+                if(phonePiece2Map == 1){
+                    phonePiece2Map = 3;
+                }else{
+                    phonePiece2Map = 7;
+                }
             }
-        }else if(phonePiece2Map == 3){
-            phonePiece2Hole = rand.nextInt(1,5);
-            phonePiece2Map =  rand.nextInt(1,3);
-            if(phonePiece2Map != 1){
-                phonePiece2Map = 2;
+            case 3 -> {
+                phonePiece2Hole = rand.nextInt(1,5);
+                phonePiece2Map =  rand.nextInt(1,3);
+                if(phonePiece2Map != 1){
+                    phonePiece2Map = 2;
+                }
             }
-        }else if(phonePiece2Map == 4){
-            phonePiece2Hole = rand.nextInt(1,9);
-            phonePiece2Map = 5;
-        }else{
-            System.err.println("ERROR: invalid map id at phone piece 2 Map spawns");
+            case 4 -> {
+                phonePiece2Hole = rand.nextInt(1,9);
+                phonePiece2Map = 5;
+            }
+            default -> System.err.println("ERROR: invalid map id at phone piece 2 Map spawns");
         }
 
         //generates the locations for the third phone piece
-        if(phonePiece3Map == 1){
-            phonePiece3Hole = rand.nextInt(1,5);
-            phonePiece3Map =  rand.nextInt(1,3);
-            if(phonePiece3Map == 1){
-                phonePiece3Map = 8;
-            }else{
-                phonePiece3Map = 9;
+        switch (phonePiece3Map) {
+            case 1 -> {
+                phonePiece3Hole = rand.nextInt(1,5);
+                phonePiece3Map =  rand.nextInt(1,3);
+                if(phonePiece3Map == 1){
+                    phonePiece3Map = 8;
+                }else{
+                    phonePiece3Map = 9;
+                }
             }
-        }else if(phonePiece3Map == 2){
-            phonePiece3Hole = rand.nextInt(1,5);
-            phonePiece3Map =  rand.nextInt(1,3);
-            if(phonePiece3Map == 1){
-                phonePiece3Map = 3;
-            }else{
-                phonePiece3Map = 7;
+            case 2 -> {
+                phonePiece3Hole = rand.nextInt(1,5);
+                phonePiece3Map =  rand.nextInt(1,3);
+                if(phonePiece3Map == 1){
+                    phonePiece3Map = 3;
+                }else{
+                    phonePiece3Map = 7;
+                }
             }
-        }else if(phonePiece3Map == 3){
-            phonePiece3Hole = rand.nextInt(1,5);
-            phonePiece3Map =  rand.nextInt(1,3);
-            if(phonePiece3Map != 1){
-                phonePiece3Map = 2;
+            case 3 -> {
+                phonePiece3Hole = rand.nextInt(1,5);
+                phonePiece3Map =  rand.nextInt(1,3);
+                if(phonePiece3Map != 1){
+                    phonePiece3Map = 2;
+                }
             }
-        }else if(phonePiece3Map == 4){
-            phonePiece3Hole = rand.nextInt(1,9);
-            phonePiece3Map = 5;
-        }else{
-            System.err.println("ERROR: invalid map id at phone piece 3 Map spawns");
+            case 4 -> {
+                phonePiece3Hole = rand.nextInt(1,9);
+                phonePiece3Map = 5;
+            }
+            default -> System.err.println("ERROR: invalid map id at phone piece 3 Map spawns");
         }
     }
 }
